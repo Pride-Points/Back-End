89a0708531955a3df743f4ae3407250aee5c5433
Enzo Medej <110927542+EnzoMedej@users.noreply.github.com>
2024-04-30 22:10:01 -0300

    revertendo minha alteraçao

--- src/main/java/com/pridepoints/api/controller/FisicaController.java
+++ src/main/java/com/pridepoints/api/controller/FisicaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class FisicaController {
     private final FisicaService fisicaService;
     private final FuncionarioService funcionarioService;
